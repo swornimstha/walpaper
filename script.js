a672306d322aa2d1e4895fc0a52f175e276ed2ec
shreemad744 <shreemad5074@proton.me>
2024-10-17 19:19:57 +0545

    api

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Hello World")
+fetch("https://wallhaven.cc/api/v1/search")
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Error loading API");
+    }
+    return response.json(); // Return parsed JSON data
+  })
+  .then((data) => {
+    console.log(data); // Log the data here
+  })
+  .catch((error) => {
+    console.error("Error:", error); // Handle any errors
+  });
